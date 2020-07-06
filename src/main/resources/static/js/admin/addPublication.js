74ae19e842fffb9b8a19e6521a93df0c930ea366
Fedunetc_Andriy <daleonandra0206@ukr.net>
2020-07-06 22:17:01 +0300

    FrontEnd: create choose categories and genre;

--- src/main/resources/static/js/admin/addPublication.js
+++ src/main/resources/static/js/admin/addPublication.js
@@ -1,97 +1,160 @@
 "use strict";
-$('#addPublicationBtn').click(function(){
-   var form
+let typePublication;
+$('#showPublicationBtn').click(function(){
+    typePublication = $('#inlineTypeSelect option:selected').text();
+    showAddPublication();
+    addBindToGenre();
+    addBindToCategories();
+    $("#addPublicationBtn").bind("click",function () {
+        alert("hj");
+        let name = $('#inputName').val(),
+            fullName = $('#inputFullName').val(),
+            director = $('#inputDirector').val(),
+            language = $('#inputLanguage').val(),
+            descrition = $('#inputDescription').val();
+
+        let genre = new Array(), i = 0;
+        $('#spanGenre').find('span').each(function () {
+                genre[i];
+                i++;
+
+        });
+        let form = new Object();
+        form.name = name;
+        form.fullName = fullName;
+        form.director = director;
+        form.language = language;
+        form.description = descrition;
+        form.genre = genre;
+
+        // let jsonForm = JSON.parse(form);
+
+        $.ajax({
+            beforeSend: function (request) {
+                request.setRequestHeader('Authorization', ('Bearer_' + localStorage.getItem('token')));
+            },
+            type: "POST",
+            dataType: "json",
+            data: form,
+            url: "/api/v1/admin/publication/anime/add",
+            success: function () {
+
+            },
+            error: function () {
+
+            }
+
+        })
+    });
+});
+
+$('#addPublicationBtn').click(function () {
+
 
 });
-$('#inputGenre').bind('input', function () {
-    $.ajax({
-            beforeSend: function(request) {
-                request.setRequestHeader('Authorization',('Bearer_' + localStorage.getItem('token')));
-            },
-            async:true,
-            type:"GET",
-            url:"/api/v1/admin/publication/genre/" + $('#inputGenre').val(),
-            success:function(res){
-                let i,j = 5,dropdownStr = "";
-                let dropdownDiv = document.getElementById("dropDownInputGenre");
-                if(res.length < j ){j = res.length}
-                for(i = 0; i < j; i++){
-                    let genre = res[i];
-                    dropdownStr += "<a class=\"dropdown-item\" value=\""+ i +"\">" + genre.genre + "</a>\n"
-                }
-             //   $('#dropDownInputGenre').empty();
-               // dropdownDiv.insertAdjacentHTML("beforeend", dropdownStr);
-                $('#dropDownInputGenre').show();
-                $('#dropDownInputCategories').hide();
-                $('#dropDownInputGenre').dropdown('update');
+function addBindToGenre() {
+    $('#inputGenre').bind('input', function () {
+        $.ajax({
+                beforeSend: function (request) {
+                    request.setRequestHeader('Authorization', ('Bearer_' + localStorage.getItem('token')));
+                },
+                async: true,
+                type: "GET",
+                url: "/api/v1/admin/publication/genre/" + $('#inputGenre').val(),
+                success: function (res) {
+                    let i, j = 5, dropdownStr = "";
+                    let dropdownDiv = document.getElementById("dropDownInputGenre");
+                    if (res.length < j) {
+                        j = res.length
+                    }
+                    for (i = 0; i < j; i++) {
+                        let genre = res[i];
+                        dropdownStr += "<a class=\"dropdown-item\" value=\"" + i + "\">" + genre.genre + "</a>\n"
+                    }
+                    $('#dropDownInputGenre').empty();
+                    dropdownDiv.insertAdjacentHTML("beforeend", dropdownStr);
+                    $('#dropDownInputGenre').show();
 
-                if(res.length === 0){
+                    if (res.length === 0) {
+                        $('#dropDownInputGenre').hide();
+                    }
+                },
+                error: function () {
                     $('#dropDownInputGenre').hide();
                 }
-            },
-            error:function () {
-                $('#dropDownInputGenre').hide();
             }
-        }
-    )
-});
-$('#dropDownInputGenre').on('click','a',function () {
-    let textSpan, genreTxt = this.text;
-    textSpan = "<span class=\"badge badge-primary\">" + genreTxt + "</span>\n";
-    $('#spanGenre').find('span').each(function (){
-        if($(this).text() === genreTxt){
-            textSpan = "";
-        }
+        )
     });
-    document.getElementById("spanGenre").insertAdjacentHTML("afterbegin",textSpan);
-    $('#dropDownInputGenre').hide();
 
-
-});
-
-$('#inputCategories').bind('input', function () {
-    $.ajax({
-        beforeSend: function(request) {
-            request.setRequestHeader('Authorization',('Bearer_' + localStorage.getItem('token')));
-        },
-        async:true,
-        type:"GET",
-        url:"/api/v1/admin/publication/categories/" + $('#inputCategories').val(),
-        success:function(res){
-            let i,j = 5,dropdownStr = "";
-            let dropdownDiv2 = document.getElementById("dropDownInputCategories");
-            if(res.length < j ){j = res.length}
-            for(i = 0; i < j; i++){
-                let genre = res[i];
-                dropdownStr += "<a class=\"dropdown-item\" value=\""+ i +"\">" + genre.categories + "</a>\n"
+    $('#dropDownInputGenre').on('click', 'a', function () {
+        let textSpan, genreTxt = this.text;
+        textSpan = "<span class=\"badge badge-primary\">" + genreTxt + "</span>\n";
+        $('#spanGenre').find('span').each(function () {
+            if ($(this).text() === genreTxt) {
+                textSpan = "";
             }
-            $('#dropDownInputCategories').empty();
-            dropdownDiv2.insertAdjacentHTML("afterbegin",dropdownStr);
-            $('#dropDownInputCategories').show();
-            $('#dropDownInputGenre').hide();
+        });
+        document.getElementById("spanGenre").insertAdjacentHTML("afterbegin", textSpan);
+        $('#dropDownInputGenre').hide();
 
-            if(res.length === 0){
-                $('#dropDownInputCategories').hide();
-            }
-        },
-        error:function () {
-            $('#dropDownInputCategories').hide();
-        }
-        }
-    )
-});
-$('#dropDownInputCategories').on('click','a',function () {
-    let textSpan, categoriesTxt = this.text;
-    textSpan = "<span class=\"badge badge-primary\">" + categoriesTxt + "</span>\n";
-    $('#spanCategories').find('span').each(function (){
-        if($(this).text() === categoriesTxt){
-            textSpan = "";
-        }
+
     });
-    document.getElementById("spanCategories").insertAdjacentHTML("afterbegin",textSpan);
-    $('#dropDownInputCategories').hide();
+    $('#inputGenre').focusout(function () {
+        $('#dropDownInputGenre').hide();
+    });
+}
+
+function addBindToCategories() {
+    $('#inputCategories').bind('input', function () {
+        $.ajax({
+                beforeSend: function (request) {
+                    request.setRequestHeader('Authorization', ('Bearer_' + localStorage.getItem('token')));
+                },
+                async: true,
+                type: "GET",
+                url: "/api/v1/admin/publication/categories/" + $('#inputCategories').val(),
+                success: function (res) {
+                    let i, j = 5, dropdownStr = "";
+                    let dropdownDiv2 = document.getElementById("dropDownInputCategories");
+                    if (res.length < j) {
+                        j = res.length
+                    }
+                    for (i = 0; i < j; i++) {
+                        let genre = res[i];
+                        dropdownStr += "<a class=\"dropdown-item\" value=\"" + i + "\">" + genre.categories + "</a>\n"
+                    }
+                    $('#dropDownInputCategories').empty();
+                    dropdownDiv2.insertAdjacentHTML("afterbegin", dropdownStr);
+                    $('#dropDownInputCategories').show();
+
+                    if (res.length === 0) {
+                        $('#dropDownInputCategories').hide();
+                    }
+                },
+                error: function () {
+                    $('#dropDownInputCategories').hide();
+                }
+            }
+        )
+    });
+    $('#inputCategories').focusout(function () {
+        $('#dropDownInputCategories').hide();
+    });
+}
+    $('#dropDownInputCategories').on('click', 'a', function () {
+        alert("jjj");
+        let textSpan, categoriesTxt = this.text;
+        textSpan = "<span class=\"badge badge-primary\">" + categoriesTxt + "</span>\n";
+        $('#spanCategories').find('span').each(function () {
+            if ($(this).text() === categoriesTxt) {
+                textSpan = "";
+            }
+        });
+        document.getElementById("spanCategories").insertAdjacentHTML("afterbegin", textSpan);
+        $('#dropDownInputCategories').hide();
 
 
-});
+    });
+
 
 
--- src/main/resources/static/js/autofillHTMLpages/adminAutoFill.js
+++ src/main/resources/static/js/autofillHTMLpages/adminAutoFill.js
@@ -1,17 +1,80 @@
 "use strict";
 // function rigthMenuAutoFill() {
-var leftMenuHtml = " <div class=\"col-12\">\n" +
-    "                <button id=\"addPublicationBtn\" type=\"button\" class=\"btn btn-outline-primary\">Додати Публікацію</button>\n" +
-    "                <button id=\"editPublicationBtn\" type=\"button\" class=\"btn btn-outline-primary\">Редагування Публікації</button>\n" +
-    "                <button id=\"getUserBtn\" type=\"button\" class=\"btn btn-outline-primary\">Отримання користувачів</button>\n" +
-    "                </div>\n" +
-    "                <div id=\"divAdmin\" class=\"col-12\" >\n" +
-    "                    \n" +
-    "                    \n" +
-    "                </div>";
+var leftMenuHtml =
+    "                        <label class=\"my-1 mr-2\" for=\"inlineTypeSelect\">Тип публікації</label>\n" +
+    "                        <select class=\"custom-select my-1 mr-sm-2\" id=\"inlineTypeSelect\">\n" +
+    "                            <option selected>Вибрати...</option>\n" +
+    "                            <option value=\"1\">Аніме</option>\n" +
+    "                            <option value=\"2\">Манга</option>\n" +
+    "                            <option value=\"3\">Гра</option>\n" +
+    "                        </select>\n" +
+    "\n" +
+    "                        <button id=\"showPublicationBtn\" class=\"btn btn-outline-primary my-1\">Додати публікацію</button>\n" +
+    "                    <button id=\"editPublicationBtn\" type=\"button\" class=\"btn btn-outline-primary\">Редагування Публікації</button>\n" +
+    "                    <button id=\"getUserBtn\" type=\"button\" class=\"btn btn-outline-primary\">Отримання користувачів</button>\n";
 var centerMenu = document.getElementById("centerMenu");
 
 centerMenu.insertAdjacentHTML("afterbegin", leftMenuHtml);
 //}
+function showAddPublication(){
+    let addPubStr = " <div class=\"form-group\">\n" +
+        "                            <label for=\"inputName\">Назва публікації</label>\n" +
+        "                            <input type=\"text\" class=\"form-control\" id=\"inputName\" placeholder=\"name\">\n" +
+        "                        </div>\n" +
+        "                        <div class=\"form-group\">\n" +
+        "                            <label for=\"inputFullName\">Повна назва публікації</label>\n" +
+        "                            <input type=\"text\" class=\"form-control\" id=\"inputFullName\" placeholder=\"fullName\">\n" +
+        "                        </div>\n" +
+        "                        <div class=\"form-group\">\n" +
+        "                            <label for=\"inputDirector\">Назва публікації</label>\n" +
+        "                            <input type=\"text\" class=\"form-control\" id=\"inputDirector\" placeholder=\"director\">\n" +
+        "                        </div>\n" +
+        "                        <div class=\"form-group\">\n" +
+        "                            <label for=\"inputLanguage\">Назва публікації</label>\n" +
+        "                            <input type=\"text\" class=\"form-control\" id=\"inputLanguage\" placeholder=\"language\">\n" +
+        "                        </div>\n" +
+        "                        <div class=\"form-group\">\n" +
+        "                            <label for=\"inputDescription\">Назва публікації</label>\n" +
+        "                            <input type=\"text\" class=\"form-control\" id=\"inputDescription\" placeholder=\"description\">\n" +
+        "                        </div>\n" +
+        "\n" +
+        "                        <div class=\"dropdown\">\n" +
+        "                            <label id=\"txtGenre\" for=\"inputGenre\">Назва публікації</label>\n" +
+        "                            <input type=\"text\" class=\"form-control\" id=\"inputGenre\" placeholder=\"жанр\">\n" +
+        "                            <div id=\"dropDownInputGenre\" class=\"dropdown-menu\" >\n" +
+        "                                <a class=\"dropdown-item\" >fafa</a>\n" +
+        "                                <a class=\"dropdown-item\" >fafa</a>\n" +
+        "                                <a class=\"dropdown-item\" >fafa</a>\n" +
+        "                            </div>\n" +
+        "\n" +
+        "                        <div id = \"spanGenre\">\n" +
+        "                            <span class=\"badge badge-primary\">Primary</span>\n" +
+        "                            <span class=\"badge badge-primary\">Primary</span>\n" +
+        "                            <span class=\"badge badge-primary\">Primary</span>\n" +
+        "                            <span class=\"badge badge-primary\">Primary</span>\n" +
+        "\n" +
+        "                        </div>\n" +
+        "                        </div>\n" +
+        "\n" +
+        "                        <div class=\"dropdown\">\n" +
+        "                            <label id=\"txtCategories\" for=\"inputCategories\">Назва публікації</label>\n" +
+        "                            <input type=\"text\" class=\"form-control\" id=\"inputCategories\" placeholder=\"категорії\">\n" +
+        "                            <div id=\"dropDownInputCategories\" class=\"dropdown-menu\" aria-labelledby=\"inputCategories\">\n" +
+        "\n" +
+        "                            </div>\n" +
+        "\n" +
+        "                            <div id = \"spanCategories\">\n" +
+        "                                <span class=\"badge badge-primary\">Primary</span>\n" +
+        "                                <span class=\"badge badge-primary\">Primary</span>\n" +
+        "                                <span class=\"badge badge-primary\">Primary</span>\n" +
+        "                                <span class=\"badge badge-primary\">Primary</span>\n" +
+        "\n" +
+        "                            </div>\n" +
+        "                        </div>\n" +
+        "                        <button id=\"addPublicationBtn\" class=\"btn btn-outline-primary my-1\">Додати публікацію</button>\n";
+    $('#centerMenu').empty();
+    centerMenu.insertAdjacentHTML("afterbegin",addPubStr);
+
+}
 
 
