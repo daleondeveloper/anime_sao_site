d1e8e2393c442e62efce2cf9c2fbe9ea26a62238
Андрій <daleonandra0206@ukr.net>
2020-08-08 11:42:56 +0300

    реалізовно отримаання id по силці сторінки і отримання публікації по  цьому id

--- src/main/resources/static/js/publication/showMain.js
+++ src/main/resources/static/js/publication/showMain.js
@@ -1,5 +1,22 @@
 "use strict"
-function showPublicationDate(){}
+function showPublicationDate(){
+    $.ajax({
+        beforeSend : function (req){
+            req.setRequestHeader('Autorization',('Bearer_' + localStorage.getItem('token')));
+        },
+        type : 'GET',
+        url : '/api/v1/publication' + getIdFromHref(),
+        success : function (){
+
+        },
+        error : function (){}
+    })
+}
 function showPublicationAvatar(){}
 function showInfoImages(){}
 function showAnimeVideo(){}
+function getIdFromHref(){
+    let path = $(location).attr('path');
+    let masOfPath = path.split('/');
+    return masOfPath[masOfPath.length - 1];
+}
