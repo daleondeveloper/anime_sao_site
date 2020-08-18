94f825fd888e97a7bf00fa35e08f6447117dbe77
Андрій <daleonandra0206@ukr.net>
2020-08-18 23:09:14 +0300

    Авто заповнення даними базу даних про публікації

---
+++ src/main/resources/database/addPublicationToDB.sql
@@ -0,0 +1,114 @@
+INSERT INTO anime_publication(id, access, create_date, description, director, full_name, language, last_update_date_time, name, release_date_time, count_series)
+VALUES
+(8,2,DATE '2019-10-29','','Ито Томохико','Мастера Меча Онлайн','RU',now(),'Мастера Меча Онлайн',now(),25),
+
+(1,2,DATE '2019-10-29','Карэн всегда чувствовала себя лишней в реальном мире. Из-за своего высокого роста девушке было очень трудно завести подруг среди ровесниц. Но всё изменилось, когда Карэн познакомилась с небезызвестной VRMMORPG — «Gun Gale Online». В мире «GGO» Карэн может сыграть за персонажа своей мечты — невысокую девочку, одетую во всё розовое, аватара её снов. Но сможет ли Карэн найти друзей в этой экшен-игре?','Сакой Масаюки','Мастера Меча Онлайн: Альтернативный «GGO»','RU',now(),'Мастера Меча Онлайн: Альтернативный «GGO»',now(),12),
+
+(2,2,DATE '2020-10-29','Нет описания','Ито Томохико','Мастера Меча Онлайн: Алисизация - Война в Подмирье 2','RU',now(),'Мастера Меча Онлайн: Алисизация - Война в Подмирье 2',now(),11),
+
+(3,2,DATE '2017-10-29','Виртуальный мир «Underworld» - больше не игра для развлечений. Последнее нападение привело к повреждению нервной системы настоящего Кадзуто Киригая. Сбой привел к тому, что его память не заблокировалась и спровоцировала конфликт с собственным виртуальным образом. Всего за несколько часов погружения Кирито проживает внутри игры около двух лет своей альтернативной жизни. Воспоминания обрывочны, а тело обездвижено, сейчас он немощный калека, неспособный говорить. Изгнанница Алиса и ее сестра берут на себя заботу о нем, даже не подозревая, что судьбы этого мира давно предрешены…
+
+В реальном мире комплекс проекта «Alicization» подвергся нападению неизвестных, все присутствующие отрезаны от внешнего мира. Их последняя надежда – Кирито, чей разум заперт внутри «Underworld». ©Nomia','Ито Томохико','Мастера Меча Онлайн: Алисизация - Война в Подмирье','RU',now(),'Мастера Меча Онлайн: Алисизация - Война в Подмирье',now(),12),
+
+(4,2,DATE '2018-10-29','Кирито просыпается в гуще огромного, фантастического леса. В попытках разузнать, как он там очутился, он встречает мальчика, который, кажется, знает его. Он должен быть обычным NPC, но его эмоции ничуть не отличаются от человеческих. Со временем к Кирито возвращаются некие воспоминания. Воспоминания из его детства, об этом мальчике и девочке с золотыми, как солнце, волосами, и именем, которое он никогда не должен забыть, — Алиса.','Ито Томохико','Мастера Меча Онлайн: Алисизация','RU',now(),'Мастера Меча Онлайн: Алисизация',now(),24),
+
+(5,2,DATE '2017-10-29','Полнометражный фильм расскажет о новой игре, для которой требуется устройство расширенной реальности под названием "Augma". Игра называется Ordinal Scale и обладает уникальными особенностями, среди которых возможность повышать свой ранг благодаря победам над монстрами и применению предметов. Augma куда компактнее Амусферы. Ordinal Scale - это ARMMORPG, эксклюзивная для Augma. Игра наделала шуму благодаря применению в ней новейших технологий, включающих реальный мир в игру в качестве игрового поля, а также возможности повышать ранг победами над монстрами и применять предметы в различных областях реального мира. Ordinal Scale (OS) - это функция в игре, и все игроки получают ранг, основанный на их порядковых номерах. Таким образом, топовые игроки обладают несокрушимой силой. В соло-PvP порядковый ранг - это важный фактор, определяющий во многом победу и поражение.','Ито Томохико','Мастера Меча Онлайн: Порядковый ранг','RU',now(),'Мастера Меча Онлайн: Порядковый ранг',now(),1),
+
+(6,2,DATE '2014-10-29','Прошел год после событий первого сезона. К Кирито обращается представитель министерства внутренних дел и коммуникаций с необычной просьбой.
+
+Речь идет об инцинденте в популярном ММО-шутере Gun Gale Online (сокращенно ГГО). Некто, под именем Death Gun, убивает топовых игроков, в результате чего они умирают и в реальной жизни. Кирито не верит, что такое возможно, но все же соглашается найти в ГГО этого странного стрелка.
+
+Бродя по миру ГГО, Кирито знакомится с девушкой-снайпером по имени Синон. С ее помощью, в надежде найти парня по имени Death Gun наш герой решает участвовать в большом турнире за звание самого лучшего стрелка ГГО.
+
+Серия 14.5 - рекап первой части второго сезона.','Ито Томохико','Мастера Меча Онлайн 2','RU',now(),'Мастера Меча Онлайн 2',now(),24),
+
+(7,2,DATE '2013-10-29','Юи хочет увидеть кита, выполняя подводное задание вместе с Кирито в Alfheim Online. Однако в ходе задания выясняется, что Лифа не умеет плавать. Асуна и другие девушки решают тренировать Сугуху в бассейне в реальном мире.' ||
+                       '
+А в это время Кадзуто кое-кого встречает...','Ито Томохико','Мастера Меча Онлайн: Дополнительное издание','RU',now(),'Мастера Меча Онлайн: Дополнительное издание',now(),1);
+INSERT INTO image_avatar(id,access,content_type,create_date,data,file_name) values
+(11,'ROLE_GUEST','image/jpg',now(),lo_import('D://season_1/main_picture_1.jpg'),'avatar'),
+(12,'ROLE_GUEST','image/jpg',now(),lo_import('D://season_1/SAO_OrdinalScale_MP1.jpg'),'avatar'),
+(13,'ROLE_GUEST','image/jpg',now(),lo_import('D://season_1/SAO_GGO_MP1.jpg'),'avatar'),
+(14,'ROLE_GUEST','image/jpg',now(),lo_import('D://season_1/SAO_Alicization_MP1.jpg'),'avatar'),
+(15,'ROLE_GUEST','image/jpg',now(),lo_import('D://season_1/SAO_S1_Special_MP1.jpg'),'avatar'),
+(16,'ROLE_GUEST','image/jpg',now(),lo_import('D://season_1/SAO_S2_MP1.jpg'),'avatar'),
+(17,'ROLE_GUEST','image/jpg',now(),lo_import('D://season_1/SAO_Alicization_MP1.jpg'),'avatar'),
+(18,'ROLE_GUEST','image/jpg',now(),lo_import('D://season_1/SAO_Alicization_MP1.jpg'),'avatar');
+UPDATE anime_publication SET avatar_img = 11 WHERE id = 8;
+UPDATE anime_publication SET avatar_img = 12 WHERE id = 5;
+UPDATE anime_publication SET avatar_img = 13 WHERE id = 1;
+UPDATE anime_publication SET avatar_img = 14 WHERE id = 2;
+UPDATE anime_publication SET avatar_img = 15 WHERE id = 7;
+UPDATE anime_publication SET avatar_img = 16 WHERE id = 6;
+UPDATE anime_publication SET avatar_img = 17 WHERE id = 3;
+UPDATE anime_publication SET avatar_img = 18 WHERE id = 4;
+INSERT INTO genre(id, genre) VALUES
+(1,'Сэйнэн'),
+(2,'Драма'),
+(3,'Приключения'),
+(4,'Романтика'),
+(5,'Любовный треугольник'),
+(6,'Триллер'),
+(7,'Виртуальная реальность'),
+(8,'Экшен'),
+(9,'Сражения на мечах'),
+(10,'Игры'),
+(11,'Исэкай');
+INSERT INTO types(id, types) VALUES
+(1,'сериал'),
+(2,'фильм');
+INSERT INTO publication_genre (publication_id, genre_id) VALUES
+(1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(1,7),(1,8),(1,9),(1,10),(1,11),
+(2,2),(2,11),(2,10),(2,5),(2,7),
+(3,1),(3,9),(3,2),(3,7),(3,3),(3,5),(3,11),(3,10),
+(4,11),
+(5,4),(5,7),(5,2),
+(6,5),(6,10),(6,11),(6,3),(6,2),(6,1),
+(7,1),(7,2),(7,3),(7,4),(7,5),(7,6),(7,7),(7,8),(7,9),(7,11),
+(8,11),(8,1),(8,3),(8,6),(8,5);
+INSERT INTO publication_types(publication_id, types_id) VALUES
+(1,1),(2,1),(3,2),(4,1),(5,1),(6,1),(7,1),(8,1);
+INSERT INTO image_publication(id,access,content_type,create_date,data,file_name,anime_publication_id) values
+(1,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_1/1.jpg'),'avatar',8),
+(2,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_1/2.jpg'),'avatar',8),
+(3,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_1/3.jpg'),'avatar',8),
+(4,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_1/4.jpg'),'avatar',8),
+(5,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_2/1.jpg'),'avatar',6),
+(6,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_2/2.jpg'),'avatar',6),
+(7,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_2/3.jpg'),'avatar',6),
+(8,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_2/4.jpg'),'avatar',6),
+(9,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_2/5.jpg'),'avatar',6),
+(10,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_2/6.jpg'),'avatar',6),
+(11,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_2/7.jpg'),'avatar',6),
+(12,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_2/8.jpg'),'avatar',6),
+(13,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_Acilization_1/1.jpg'),'avatar',4),
+(14,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_Acilization_1/2.jpg'),'avatar',4),
+(15,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_Acilization_1/3.jpg'),'avatar',4),
+(16,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_Acilization_1/4.jpg'),'avatar',4),
+(17,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_Acilization_1/5.jpg'),'avatar',4),
+(18,'ROLE_GUEST','image/png',now(),lo_import('D://info_publication/SAO_Acilization_2/1.png'),'avatar',3),
+(19,'ROLE_GUEST','image/png',now(),lo_import('D://info_publication/SAO_Acilization_2/2.png'),'avatar',3),
+(20,'ROLE_GUEST','image/png',now(),lo_import('D://info_publication/SAO_Acilization_2/3.png'),'avatar',3),
+(21,'ROLE_GUEST','image/png',now(),lo_import('D://info_publication/SAO_Acilization_2/4.png'),'avatar',3),
+(22,'ROLE_GUEST','image/png',now(),lo_import('D://info_publication/SAO_Acilization_2/5.png'),'avatar',3),
+(23,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_Acilization_3/1.jpg'),'avatar',2),
+(24,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_Acilization_3/2.jpg'),'avatar',2),
+(25,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_Acilization_3/3.jpg'),'avatar',2),
+(26,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_GGO/1.jpg'),'avatar',1),
+(27,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_GGO/2.jpg'),'avatar',1),
+(28,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_GGO/3.jpg'),'avatar',1),
+(29,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_GGO/4.jpg'),'avatar',1),
+(30,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_GGO/5.jpg'),'avatar',1),
+(31,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_GGO/6.jpg'),'avatar',1),
+(32,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_Ordinal/1.jpg'),'avatar',5),
+(33,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_Ordinal/2.jpg'),'avatar',5),
+(34,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_Ordinal/3.jpg'),'avatar',5),
+(35,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_Ordinal/4.jpg'),'avatar',5),
+(36,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_SPECIAL/1.jpg'),'avatar',7),
+(37,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_SPECIAL/2.jpg'),'avatar',7),
+(38,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_SPECIAL/3.jpg'),'avatar',7),
+(39,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_SPECIAL/4.jpg'),'avatar',7),
+(40,'ROLE_GUEST','image/jpg',now(),lo_import('D://info_publication/SAO_SPECIAL/5.jpg'),'avatar',7);
+
+
+
